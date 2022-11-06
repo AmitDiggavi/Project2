b7551d1422ecc8681b90fd6cacde729b27776acb
adubov1 <adubov@cps.edu>
2022-11-06 17:28:01 -0600

    add product and productloader

---
+++ Product.java
@@ -0,0 +1,35 @@
+public class Product implements IProduct {
+    String name;
+    String category;
+    float price;
+    int quantity;
+    String UPC;
+
+    public Product(String name, String category, float price, int quantity, String UPC) {
+        this.name = name;
+        this.category = category;
+        this.price = price;
+        this.quantity = quantity;
+        this.UPC = UPC;
+    }
+
+    public String getName() {
+        return name;
+    };
+
+    public String getCategory() {
+        return category;
+    };
+
+    public double getPrice() {
+        return price;
+    };
+
+    public int getQuantity() {
+        return quantity;
+    };
+
+    public String getUPC() {
+        return UPC;
+    };
+}
